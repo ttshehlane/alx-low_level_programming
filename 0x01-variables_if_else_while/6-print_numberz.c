040fddec1b1f77f5b73e28126d10a95208c2da8e
Thuba <ttshehlane@outlook.com>
2022-09-13 12:58:34 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - main block area
+ * Return: 0
+ */
+int main(void)
+{
+	int ch;
+
+	/* print lowercase letters */
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
+
