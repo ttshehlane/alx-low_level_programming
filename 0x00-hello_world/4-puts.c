f342d04613f277b06e2789e06cfaf942b48c7684
Thuba <ttshehlane@outlook.com>
2022-09-13 11:37:12 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-#Include <stdio.h>
+#include <stdio.h>
 /**
- *  * main - main block
- *   * Return: 0
- *    */
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
