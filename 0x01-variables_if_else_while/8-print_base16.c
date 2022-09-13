797218ba56dfb7052adf0bce01a1498c33f350e0
Thuba <ttshehlane@outlook.com>
2022-09-13 13:03:58 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - main block area
+ * Return: 0
+ */
+int main(void)
+{
+	int ch;
+
+	/* print lowercase letters */
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
