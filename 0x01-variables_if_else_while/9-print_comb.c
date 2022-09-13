4acc0dfa08f249837629b96f7b54d05ce5dfa309
Thuba <ttshehlane@outlook.com>
2022-09-13 13:13:20 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - main block area
+ * Return: 0
+ */
+int main(void)
+{
+	int ch;
+
+	/* print lowercase letters */
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+		if (ch < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}	
+	}
+	putchar('\n');
+	return (0);
+}
+
