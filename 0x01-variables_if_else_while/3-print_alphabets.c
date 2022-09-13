3923e01509c8b600a44d546ba5c4f5e3d30b688c
Thuba <ttshehlane@outlook.com>
2022-09-13 12:40:12 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - main block area
+ * Return: 0
+ */
+int main(void)
+{
+	int ch;
+
+	/* print lowercase letters */
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
