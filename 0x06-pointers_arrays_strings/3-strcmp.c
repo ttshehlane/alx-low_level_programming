5bcc99da24ddd90e6ed1705ca96fdd6e227dd8d4
Thuba <ttshehlane@outlook.com>
2022-09-21 14:50:47 -0700

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - a funtion that compare two string
+ * @s1: variable to be compared with @s2
+ * @s2: variable to be compared with @s1
+ *
+ * Return: Always 0 (success)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	if (s1 == s2)
+		i = 0;
+	if (s1 < s2)
+		i = -24;
+	if (s1 > s2)
+		i = 24;
+	return (i);
+}
