99c561db80b18beee71f51b1bcfbec85ac1101ed
Thuba <ttshehlane@outlook.com>
2022-09-21 16:33:51 -0700

    save

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * reverse_array - funtion to reverse the input text
+ * @a: array of text to be reversed
+ * @n: the length of the array
+ *
+ * Return: Always 0 (success)
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	for (i = n; i <= 0 ; i--)
+	{
+
+	}
+}
