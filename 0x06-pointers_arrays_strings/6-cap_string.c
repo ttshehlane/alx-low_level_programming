a7de47ae29b14ed9898964603804e73e2149a988
Thuba <ttshehlane@outlook.com>
2022-09-22 17:25:56 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,8 +8,9 @@
  */
 char *cap_string(char *str)
 {
-	int i;
+	int i, j;
 	int delt = 'a' - 'A';
+	char c[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
@@ -20,25 +21,26 @@ char *cap_string(char *str)
 				str[i] -= delt;
 			continue; /*carry on with the loop*/
 		}
-		if ((str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || str[i] == ';' || 
-		str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')) 
-			/*checking for word separtors: space, tabulation, new line, ,, ;, ., !, ?, \", (, ), {, and }*/
-		{
-			/*if the above are found we go ahead to check the next character*/
-			i++;
-			/*Check if this character is lower case and if yes we go ahead and convert it to uppercase*/
-			if (str[i] >= 'a' && str[i] <= 'z')
-			{
-				str[i] -= delt;
-				continue; /* carry on with the loop*/
+		for (j = 0; c[j] != '\0'; j++) 
+		{/*checking for word separtors: space, tabulation, new line, ,, ;, ., !, ?, \", (, ), {, and }*/
+			if (str[i] == c[j])
+			{/*if the above are found we go ahead to check the next character*/
+				i++;
+				/*Check if this character is lower case and if yes we go ahead and convert it to uppercase*/
+				if (str[i] >= 'a' && str[i] <= 'z')
+				{
+					str[i] -= delt;
+					break; /* break the loop*/
+				}
 			}
-		}
-		else
-		{
-			/*now am converting all the uppercase character that do not meet the above requiments to lowercase*/
-			if (str[i] >= 'A' && str[i] <= 'Z')
-				str[i] += delt;
-		}		
+			else
+			{
+				/*now am checking if our string is uppercase and will need to convert it to lower case*/
+				if(str[i] >= 'A' && str[i] <= 'Z')
+					str[i] += delt;
+				break;
+			}
+		}	
 	}
 	return (str);
 }
