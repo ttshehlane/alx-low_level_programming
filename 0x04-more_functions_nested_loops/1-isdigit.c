6e9013f04c22ddbd5db12473f26852388213e7f3
Thuba <ttshehlane@outlook.com>
2023-02-16 10:04:25 -0800

    function that checks for a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if a character is upper
+ *
+ * Description: Prints the the input with _putchar
+ * @c: integer
+ * Return: int
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 0)
+		return (1);
+	else
+		return (0);
+}
