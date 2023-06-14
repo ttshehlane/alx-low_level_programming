b569981d3baf199148eacc6c3cda9dedec52f826
Thuba <ttshehlane@outlook.com>
2023-06-14 15:33:21 -0700

    code changes to fix a failed check

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,28 +14,20 @@ char *str_concat(char *s1, char *s2)
 	unsigned int len, lens1, a = 0;
 	char *p;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 == "";
+
 	lens1 = strlen(s1);
 	len = lens1 + strlen(s2);
-	p = (char *)malloc(sizeof(s1) + sizeof(s2) - 4);
+	p = (char *)malloc(sizeof(s1) + sizeof(s2) + 1);
 	if (p == NULL)
 		return (NULL);
-	if (s1 == NULL)
-	{
-		p[a] = ' ';
-		a++;
-	}
-	else
-	{
-		for (a = 0; a < strlen(s1); a++)
-			p[a] = s1[a];
-	}
-	if (s2 == NULL)
-		p[lens1] = ' ';
-	else
-	{
-		for (a = lens1; a < len; a++)
-			p[a] = s2[a - lens1];
-	}
+	for (a = 0; a < strlen(s1); a++)
+		p[a] = s1[a];
+	for (a = lens1; a < len; a++)
+		p[a] = s2[a - lens1];
 	p[len] = '\0';
 	return (p);
 }
