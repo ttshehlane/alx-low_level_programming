206c33ab8c9a3db127d345aed22a9d1aae57f245
Thuba <ttshehlane@outlook.com>
2023-06-19 03:19:19 -0700

    shortened some of the if statement to meet betty requirments

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,13 +15,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *c;
 	unsigned int s1Len, s2Len, count;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-	{
-		s2 = "";
-		n = 1;
-	}
+	s1 = (s1 == NULL) ? "" : s1;
+	s2 = (s2 == NULL) ? "" : s2;
+	n = (s2[0] == '\0') ? 1 : n;
 	s1Len = strlen(s1);
 	s2Len = strlen(s2);
 	if (n >= s2Len)
@@ -46,13 +42,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		for (count = 0; count < (s1Len + n); count++)
 		{
 			if (count < s1Len)
-			{
 				c[count] = s1[count];
-			}
 			else
 				c[count] = s2[count - s1Len];
-		}
 		c[s1Len + n] = '\0';
+		}
 	}
 	return (c);
 }
