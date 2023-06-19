9729b2ba0ce7645c428aa2879e2a11f27e1d58af
Thuba <ttshehlane@outlook.com>
2023-06-19 05:23:33 -0700

    test

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,9 +15,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *c;
 	unsigned int s1Len, s2Len, count;
 
-	s1 = (s1 == NULL) ? " " : s1;
-	s2 = (s2 == NULL) ? " " : s2;
-	n = (s2[1] == '\0' || n == 0) ? 1 : n;
+	if (s1 == NULL)
+		s1 = " ";
+	if (s2 == NULL)
+	{
+		s2 = " ";
+		n = 1;
+	}
 	s1Len = strlen(s1);
 	s2Len = strlen(s2);
 	if (n >= s2Len)
