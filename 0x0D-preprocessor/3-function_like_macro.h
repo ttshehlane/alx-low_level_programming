eaf7c31a66e77e821908e821a5e94ce9fc0ee6e2
Thuba <ttshehlane@outlook.com>
2023-06-20 08:20:31 -0700

    fixing my guard statements

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,8 +1,8 @@
-#ifndef 3_FUNCTION_LIKE_MACRO_H
-#define 3_FUNCTION_LIKE_MACRO_H
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
 #ifndef ABS
 #define ABS(x) ((x < 0) ? (-x) : x)
 #endif /* ABS(x) */
 
-#endif /* 3_FUNCTION_LIKE_MACRO_H */
+#endif /* FUNCTION_LIKE_MACRO_H */
