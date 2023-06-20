c6f1df636f12c8e80da8177379c24b932ab76614
Thuba <ttshehlane@outlook.com>
2023-06-20 02:55:00 -0700

    removed cast from malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	total = nmemb * size;
-	c = (char *)malloc(total);
+	c = malloc(total);
 	if (c == NULL)
 		return (NULL);
 	for (a = 0; a < nmemb; a++)
