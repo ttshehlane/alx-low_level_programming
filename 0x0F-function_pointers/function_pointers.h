c52ae77211cc3f4bbe4c2e12d0e19095d005d371
Thuba <ttshehlane@outlook.com>
2023-06-21 18:08:45 -0700

    added the functions used in the main file to header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -6,5 +6,7 @@
 int _putchar(char c);
 
 void print_name(char *name, void (*f)(char *));
+void print_name_as_is(char *name);
+void print_name_uppercase(char *name);
 
 #endif /* FUNCTION_POINTERS_H */
