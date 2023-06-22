7961bbce5831519294cdd9b914bf65e00d887f0e
Thuba <ttshehlane@outlook.com>
2023-06-22 05:08:51 -0700

    program that performs simple operations.

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,23 @@
+#include "3-calc.h"
+#include <stdlib.h>
+/**
+ * op_add - calculate sum of two 2 ints
+ * @a: value one
+ * @b: value two
+ * Return: integer
+ */
+int op_add(int a, int b)
+{
+
+}
+int op_sub(int a, int b)
+{}
+
+int op_mul(int a, int b)
+{}
+
+int op_div(int a, int b)
+{}
+
+int op_mod(int a, int b)
+{}
