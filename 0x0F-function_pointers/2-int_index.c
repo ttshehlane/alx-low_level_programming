85f222c48cabdee7bf583a386ff3cf0c63a6e7e2
Thuba <ttshehlane@outlook.com>
2023-06-21 19:40:38 -0700

    searches for an integer.

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * int_index - searches for an integer
+ * @array: the array of integers
+ * @size: number of elements in the array
+ * @cmp: pointer funtion used to compare val
+ * Return: -1 if val matches
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int b = 0, a = 0;
+
+	if(!array || size <= 0 || !cmp)
+		return (-1);
+	for (a = 0; a < size; a++)
+	{
+		b = cmp(array[a]);
+		if (b == 1)
+			break;
+	}
+	return (a);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -11,5 +11,9 @@ void print_name_uppercase(char *name);
 void array_iterator(int *array, size_t size, void (*action)(int));
 void print_elem(int elem);
 void print_elem_hex(int elem);
+int int_index(int *array, int size, int (*cmp)(int));
+int is_98(int elem);
+int is_strictly_positive(int elem);
+int abs_is_98(int elem);
 
 #endif /* FUNCTION_POINTERS_H */
