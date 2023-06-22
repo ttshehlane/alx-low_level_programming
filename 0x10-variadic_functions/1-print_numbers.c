866b988db9b81a387470f3a7e373e14d8c8bb204
Thuba <ttshehlane@outlook.com>
2023-06-22 09:20:54 -0700

    fixed betty error and somecode issues

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,21 +10,18 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
-	
+
 	va_start(ap, n);
-	
-	if (n == 0)
-		return;
 	if (separator == NULL)
 		for (i = 0; i < n; i++)
 			printf("%d", va_arg(ap, int));
 	else
 	{
 		for (i = 0; i < n; i++)
-		{	
+		{
 			printf("%d", va_arg(ap, int));
-			if(i < n - 1)
-				printf("%s",separator);
+			if (i < n - 1)
+				printf("%s", separator);
 		}
 	}
 	va_end(ap);
