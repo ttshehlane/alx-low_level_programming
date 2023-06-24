c83dae4be473caef19efabb82c774140672798f1
Thuba <ttshehlane@outlook.com>
2023-06-24 08:57:13 -0700

    fixed the separators

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,13 +9,13 @@
 void print_all(const char * const format, ...)
 {
 	va_list all;
-	unsigned int c = 0;
+	unsigned int c = 0, skip;
 	char *str;
 
 	va_start(all, format);
-	c = 0;
 	while (format[c])
 	{
+		skip = 2;
 		switch (format[c])
 		{
 		case 'c':
@@ -37,8 +37,11 @@ void print_all(const char * const format, ...)
 			printf("(nil)");
 			break;
 		default:
+			skip = 0;
 			break;
 		}
+		if (skip && format[c + 1])
+			printf(", ");
 		c++;
 	}
 	va_end(all);
