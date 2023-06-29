54d1da1c63192617ca538baef857198f5de7b425
Thuba <ttshehlane@outlook.com>
2023-06-29 14:35:14 -0700

    const keyword in the function indicates that the function promises h will not be editted and is not null

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer of struct
+ * Return: Number of element long
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int i = 0;
+
+	while (h != NULL)
+	{
+		i++;
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			return (i);
+		}
+		printf("[%u} %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (i);
+}
