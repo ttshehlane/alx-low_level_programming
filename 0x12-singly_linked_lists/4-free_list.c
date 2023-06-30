2793ef16f549b9f98e353c17427aad09eb3d48af
Thuba <ttshehlane@outlook.com>
2023-06-29 19:37:38 -0700

    freed the head

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,10 +9,12 @@ void free_list(list_t *head)
 {
 	list_t *current;
 
-	while(head != NULL)
+	while (head != NULL)
 	{
 		current = head;
 		head = head->next;
+		free(current->str);
 		free(current);
 	}
+	free(head);
 }
