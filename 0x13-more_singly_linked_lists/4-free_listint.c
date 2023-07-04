42f2c80b15bad7209d43a5fc06a8aaed5856f09e
Thuba <ttshehlane@outlook.com>
2023-07-04 07:58:34 -0700

    a function that frees a

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a list
+ * @head: head of the list
+ * Return: Nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+	
+	if (head == NULL)
+		return;
+	else
+	{
+		while (head)
+		{
+			temp = head->next;
+			free(head);
+			head = temp;
+		}
+	}
+}
