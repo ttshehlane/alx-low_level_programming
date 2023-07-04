f8a7b02e714b9a4bbc93cb1002c65e327df3569a
Thuba <ttshehlane@outlook.com>
2023-07-04 08:04:59 -0700

    a function that frees a

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,16 +8,14 @@
 void free_listint(listint_t *head)
 {
 	listint_t *temp;
-	
+
 	if (head == NULL)
 		return;
-	else
+
+	while (head)
 	{
-		while (head)
-		{
-			temp = head->next;
-			free(head);
-			head = temp;
-		}
+		temp = head->next;
+		free(head);
+		head = temp;
 	}
 }
