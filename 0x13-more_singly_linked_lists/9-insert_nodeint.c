e505c258ae49a32e88094c69e03afba906a74081
Thuba <ttshehlane@outlook.com>
2023-07-04 20:02:06 -0700

    fixed betty issues

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,14 +19,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = NULL;
 	current = *head;
-	
+
 	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
-	while (current != NULL && count < idx -1)
+	while (current != NULL && count < idx - 1)
 	{
 		current = current->next;
 		count++;
