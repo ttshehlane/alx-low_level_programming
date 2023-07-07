b2a82339040fbcd9f76fba74394ed9e67b701cf6
Thuba <ttshehlane@outlook.com>
2023-07-07 15:17:52 -0700

    converting long int to binary value

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - prints a binary value of a number
+ * @n: the value to be converted to binary
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	int first_non_zerobit = 0;
+	char bit;
+
+	while (mask > 0)
+	{/*checking if remaind will be 0 or 1*/
+		if (n & mask)
+			bit = '1';
+		else
+			bit = '0';
+		if (bit == '1')
+			first_non_zerobit = 1;
+		if (first_non_zerobit)
+			_putchar(bit);
+		mask >>= 1;
+	}
+	_putchar('\n');
+}
