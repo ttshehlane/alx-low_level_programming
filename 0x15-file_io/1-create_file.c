b4983c2ec720c1f9b7235e03b4ae0de3460dab6e
Thuba <ttshehlane@outlook.com>
2023-07-11 16:33:13 -0700

    fixed betty issue

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -33,7 +33,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		n_written = write(fd, text_content, _strlen(text_content));
 		if (n_written == -1)
-		{	
+		{
 			close(fd);
 			return (-1);
 		}
