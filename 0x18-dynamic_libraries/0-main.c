b9ca1f3f08067d4ee143cd511fdcb61633b3a706
Thuba Tshehlane <ttshehlane@outlook.com>
2023-09-18 06:11:14 -0700

    creating a dynamic library

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    printf("%ld\n", strlen("My Dyn Lib"));
+    return (EXIT_SUCCESS);
+}
---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+char *_strncat(char *dest, char *src, int n)
+{
+	return (dest);
+}
+
